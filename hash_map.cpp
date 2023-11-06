ffa7c7e7129f97da8164bd7ce58ec7cc9e830ff0
Sarah Grossheim <sgrossheim@una.edu>
2023-11-06 17:14:35 -0600

    Almost completed hash map with insert, search, and delete

--- hash_map.cpp
+++ hash_map.cpp
@@ -1,4 +1,64 @@
+// Author: Sarah Grossheim
 #include <iostream>
-#include "HashTable.h"
+#include <map>
+#include <iterator>
 
 using namespace std;
+
+int main() {
+	// Declare a map with string keys and int values
+	map<string, int> hashMap;
+
+	// Insert elements
+	hashMap["Thousand Island"] = 0;
+	hashMap["Ranch Dressing"] = 1;
+	hashMap["Italian       "] = 2;
+	hashMap["Blue Cheese"] = 3;
+	hashMap["Vinaigrette"] = 4;
+
+	// Another way to insert elements in a map
+	hashMap.insert(pair<string, int>("Caesar Dressing", 5));
+
+	// Access and print values in the map using an iterator
+	map<string, int>::iterator itr;
+	cout << "Printing keys and values... " << endl << endl;	
+	cout << "Key:			Value:" << endl;
+	for(itr = hashMap.begin(); itr != hashMap.end(); itr++){
+		cout << itr->first << "		" << itr->second << endl;
+	}
+	cout << endl << endl;
+
+	// Search for two specific keys
+	string salad1 = "Thousand Island";
+	string salad2 = "Ketchup";
+	
+	cout << "Searching for " << salad1 << "..." << endl;
+	if(hashMap.find(salad1) != hashMap.end())
+		cout << salad1 << " is in the map." << endl << endl;
+	else 
+		cout << salad1 << " is not in the map." << endl << endl;
+	
+	cout << "Searching for " << salad2 << "..." << endl;
+	if(hashMap.find(salad2) != hashMap.end())
+		cout << salad2 << " is in the map." << endl << endl;
+	else
+		cout << salad2 << " is not in the map" << endl << endl;
+	
+	// Delete the worst salad dressing (Vinaigrette)
+	cout << "Deleting the worst salad dressing..." << endl << endl;
+	string worstSalad;
+	worstSalad = hashMap.erase("Vinaigrette");
+
+	// Display map with deleted item
+	cout << "Printing keys and values after deletion..." << endl << endl;
+	cout << "Key: 			Value:" << endl;
+	for(itr = hashMap.begin(); itr != hashMap.end(); itr++){
+		cout << itr->first << "		" << itr->second << endl;
+	}
+
+	
+
+
+}
+
+
